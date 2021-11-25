cf71680b0c99a285f10dcf92896d331f96909817
0lbap <pablo.laviron@etu.umontpellier.fr>
2021-11-25 21:39:05 +0100

    Page d'accueil incroyable

--- index.php
+++ index.php
@@ -9,91 +9,112 @@
     <title>Projet Web</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-sm navbar-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="https://via.placeholder.com/30x30.png?text=LOGO" alt="LOGO">
-                Projet Web
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Accueil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="recherche.php">Produits</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">À propos</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <?php
-                                if(isset($_SESSION['email'])){
-                                    echo 'Bonjour, ' . $_SESSION['prenom'];
-                                } else {
-                                    echo 'Compte';
-                                }
-                            ?>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <?php
-                                if(isset($_SESSION['email'])){
-                                    echo '<li><a class="dropdown-item" href="panier.php">Mon panier</a></li>';
-                                    echo '<li><a class="dropdown-item" href="commandes.php">Mes commandes</a></li>';
-                                    echo '<li><hr class="dropdown-divider"></li>';
-                                    echo '<li><a class="dropdown-item" href="deconnexion.php">Déconnexion</a></li>';
-                                } else {
-                                    echo '<li><a class="dropdown-item" href="connexion.php">Se connecter</a></li>';
-                                    echo '<li><a class="dropdown-item" href="inscription.php">S\'inscrire</a></li>';
-                                }
-                            ?>
+    <div class="first-screen">
+        <div class="nav-gradient">
+            <nav class="navbar navbar-expand-sm navbar-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="https://via.placeholder.com/30x30.png?text=LOGO" alt="LOGO">
+                        Projet Web
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav me-auto">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Accueil</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="recherche.php">Produits</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">À propos</a>
+                            </li>
                         </ul>
-                    </li>
-                </ul>
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <?php
+                                        if(isset($_SESSION['email'])){
+                                            echo 'Bonjour, ' . $_SESSION['prenom'];
+                                        } else {
+                                            echo 'Compte';
+                                        }
+                                    ?>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <?php
+                                        if(isset($_SESSION['email'])){
+                                            echo '<li><a class="dropdown-item" href="panier.php">Mon panier</a></li>';
+                                            echo '<li><a class="dropdown-item" href="commandes.php">Mes commandes</a></li>';
+                                            echo '<li><hr class="dropdown-divider"></li>';
+                                            echo '<li><a class="dropdown-item" href="deconnexion.php">Déconnexion</a></li>';
+                                        } else {
+                                            echo '<li><a class="dropdown-item" href="connexion.php">Se connecter</a></li>';
+                                            echo '<li><a class="dropdown-item" href="inscription.php">S\'inscrire</a></li>';
+                                        }
+                                    ?>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="p-5 pt-0">
+            <h2 class="text-left fw-bold title">Exprimez<br>votre style.</h2>
+            <a href="recherche.php" class="btn btn-light rounded-pill m-2">Découvrez nos produits</a>
+        </div>
+        <div class="container">
+            <div class="row pb-5 chevron-down">
+                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
             </div>
         </div>
-    </nav>
-    <main class="container">
-        <h2 class="text-left fw-bold display-1 title">Exprimez<br>votre style.</h2>
-        <div class="row p-5">
-            <div class="cardcontainer">
-                <div class="card col-lg-3 border-0 infocard" id="ic1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-truck"><rect x="1" y="3" width="15" height="13"></rect><polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon><circle cx="5.5" cy="18.5" r="2.5"></circle><circle cx="18.5" cy="18.5" r="2.5"></circle></svg>
-                    <div class="card-body">
-                        <h5>Livraison rapide</h5>
+    </div>
+    <div class="second-screen">
+        <div class="row infocard p-4" id="ic1">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-3 col-lg-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-truck"><rect x="1" y="3" width="15" height="13"></rect><polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon><circle cx="5.5" cy="18.5" r="2.5"></circle><circle cx="18.5" cy="18.5" r="2.5"></circle></svg>
+                    </div>
+                    <div class="col-md-9">
+                        <h3>Livraison rapide</h3>
                         <p>Nos livraisons sont parmis les plus sûres et les plus rapides du marché. En effet, près de 90% de nos clients en sont satifsait !</p>
                     </div>
                 </div>
-                <div class="card col-lg-3 border-0 infocard" id="ic2">
-                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
-                    <div class="card-body">
-                        <h5>Produits de qualité</h5>
+            </div>
+        </div>
+        <div class="row infocard p-4" id="ic2">
+            <div class="col-md-6"></div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-3 col-lg-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
+                    </div>
+                    <div class="col-md-9">
+                        <h3>Produits de qualité</h3>
                         <p>Nos produits sont neufs et achettés dirrectement à l'usine. De plus, ils sont transportés par nos transporteurs certifiés, ce qui réduit le risque de produits défectueux.</p>
                     </div>
                 </div>
-                <div class="card col-lg-3 border-0 infocard" id="ic3">
-                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-                    <div class="card-body">
-                        <h5>Service après vente</h5>
+                
+            </div>
+        </div>
+        <div class="row infocard p-4" id="ic3">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-3 col-lg-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+                    </div>
+                    <div class="col-md-9">
+                        <h3>Service après vente</h3>
                         <p>Vous n'êtes pas satifsait par un produit acheté chez nous ? Ce n'est pas grave, vous avez 2 mois pour le renvoyer et ainsi bénéficier d'un coupon de réduction !</p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row pb-5">
-            <div class="text-center">
-                <h2 id="discoverh">Prêt à essayer ?</h2>
-                <br>
-                <a href="recherche.php" class="btn btn-dark rounded-pill" id="discoverbtn">Découvrez nos produits !</a>
-            </div>
-        </div>
-    </main>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
