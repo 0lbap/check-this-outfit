d0b7aa48b7e6eac0738e8e45d030483de042e789
0lbap <pablo.laviron@etu.umontpellier.fr>
2021-12-04 14:15:49 +0100

    Correction nom prénom donnees.sql

--- donnees.sql
+++ donnees.sql
@@ -1,5 +1,5 @@
-INSERT INTO Clients (email, motDePasse, nom, prenom, ville, adresse, telephone) VALUES ("pablo.laviron@etu.umontpellier.fr", "$2y$10$uqCQMwn592CuQeaa1rdZA.R3/2kNQn85DaBKbFlNNuy5gmEklRnOm", "Pablo", "Laviron", "Montpellier", "4 Impasse de la fontaine", "0712345678"); -- Mdp = password
-INSERT INTO Clients (email, motDePasse, nom, prenom, ville, adresse, telephone) VALUES ("axel.cazorla@etu.umontpellier.fr", "$2y$10$2r3wtOjStccDNPLV0l53x.f4Yq2MlCjdfzmCXaQz0piVPn9JTNGyG", "Axel", "Cazorla", "Montpellier", "103 Rue de l école", "0723456789"); -- Mdp = 12345678
+INSERT INTO Clients (email, motDePasse, nom, prenom, ville, adresse, telephone) VALUES ("pablo.laviron@etu.umontpellier.fr", "$2y$10$uqCQMwn592CuQeaa1rdZA.R3/2kNQn85DaBKbFlNNuy5gmEklRnOm", "Laviron", "Pablo", "Montpellier", "4 Impasse de la fontaine", "0712345678"); -- Mdp = password
+INSERT INTO Clients (email, motDePasse, nom, prenom, ville, adresse, telephone) VALUES ("axel.cazorla@etu.umontpellier.fr", "$2y$10$2r3wtOjStccDNPLV0l53x.f4Yq2MlCjdfzmCXaQz0piVPn9JTNGyG", "Cazorla", "Axel", "Montpellier", "103 Rue de l école", "0723456789"); -- Mdp = 12345678
 INSERT INTO Commandes (dateCommande, email, etat) VALUES ("2020-10-12", "pablo.laviron@etu.umontpellier.fr", "En cours");
 INSERT INTO Commandes (dateCommande, email, etat) VALUES ("2020-06-24", "axel.cazorla@etu.umontpellier.fr", "Livré");
 INSERT INTO Lignescommandes (idCommande, idProduit, quantite, montant) VALUES (1, 2, 1, 890.99);
