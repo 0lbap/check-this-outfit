42233d0c4bc0bfdb9e7232c744fadecc338beeaf
Pablo L <pablo.jovelaviron@orange.fr>
2021-12-08 16:08:35 +0100

    Update commandes.php

--- commandes.php
+++ commandes.php
@@ -48,12 +48,24 @@ error_reporting(E_ALL);
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                             <?php
                                 if(isset($_SESSION['user'])){
-                                    echo '<li><a class="dropdown-item" href="commandes.php">Mes commandes</a></li>';
+                                    echo '<li><a class="dropdown-item" href="commandes.php">';
+                                    echo '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-bag"><path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path><line x1="3" y1="6" x2="21" y2="6"></line><path d="M16 10a4 4 0 0 1-8 0"></path></svg>';
+                                    echo '<span class="m-1 mt-0 mb-0"></span>';
+                                    echo 'Mes commandes</a></li>';
                                     echo '<li><hr class="dropdown-divider"></li>';
-                                    echo '<li><a class="dropdown-item" href="deconnexion.php">Déconnexion</a></li>';
+                                    echo '<li><a class="dropdown-item" href="deconnexion.php">';
+                                    echo '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-power"><path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path><line x1="12" y1="2" x2="12" y2="12"></line></svg>';
+                                    echo '<span class="m-1 mt-0 mb-0"></span>';
+                                    echo 'Déconnexion</a></li>';
                                 } else {
-                                    echo '<li><a class="dropdown-item" href="connexion.php">Se connecter</a></li>';
-                                    echo '<li><a class="dropdown-item" href="inscription.php">S\'inscrire</a></li>';
+                                    echo '<li><a class="dropdown-item" href="connexion.php">';
+                                    echo '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-in"><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path><polyline points="10 17 15 12 10 7"></polyline><line x1="15" y1="12" x2="3" y2="12"></line></svg>';
+                                    echo '<span class="m-1 mt-0 mb-0"></span>';
+                                    echo 'Se connecter</a></li>';
+                                    echo '<li><a class="dropdown-item" href="inscription.php">';
+                                    echo '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-plus"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg>';
+                                    echo '<span class="m-1 mt-0 mb-0"></span>';
+                                    echo 'S\'inscrire</a></li>';
                                 }
                             ?>
                         </ul>
@@ -82,38 +94,42 @@ error_reporting(E_ALL);
                         die('Erreur : '.$e->getMessage());
                     }
                 
-                $getcommandes=$bdd->prepare("SELECT *,DATE_FORMAT(dateCommande, '%d/%m/%Y') as dateCommandeF FROM Lignescommandes,Commandes,Produits WHERE Lignescommandes.idCommande = Commandes.idCommande AND Lignescommandes.idProduit = Produits.idProduit AND email=? ORDER BY dateCommande DESC");
+                $getcommandes=$bdd->prepare("SELECT *,DATE_FORMAT(dateCommande, '%d/%m/%Y') as dateCommandeF FROM Commandes WHERE email=? ORDER BY dateCommande DESC");
                 $getcommandes->execute(array($email));
-                $commandesdata=$getcommandes->fetch();
+                $commandesdata=$getcommandes->fetchAll();
 
                 if($getcommandes->rowcount()==0){
                     echo '<div class="alert alert-primary">Vous n\'avez pas encore passé de commandes. <a class="alert-link" href="panier.php">Voir mon panier</a></div>';
                     die();
                 }
-
-                var_dump($commandesdata);
             ?>
-            <h2 class="pb-4 pt-3">Vos commandes</h2>
-            <table class="table table-striped table-hover">
-                <tr>
-                    <th>Numéro de commande</th>
-                    <th>Articles</th>
-                    <th>Montant total</th>
-                    <th>Commandé le</th>
-                    <th>État</th>
-                </tr>
+            <h3 class="display-6 mb-4 mt-3">Vos commandes</h3>
                 <?php
-                    for($i=0;$i<$getcommandes->rowcount();$i++){
+                    foreach($commandesdata as $commande){
+                        echo '<div class="card shadow p-4 mb-5">';
+                        echo '<table class="table">';
                         echo '<tr>';
-                        echo '<td>' . $commandesdata['idCommande'] . '</td>';
-                        echo '<td>x' . $commandesdata['quantite'] . ' ' . $commandesdata['marque'] . ' ' . $commandesdata['nom'] . '</td>';
-                        echo '<td>' . $commandesdata['montant'] . '€</td>';
-                        echo '<td>' . $commandesdata['dateCommandeF'] . '</td>';
-                        echo '<td>' . $commandesdata['etat'] . '</td>';
+                        echo '<th class="col-2 table-secondary">Commande n°13908130' . $commande['idCommande'] . '</th>';
+                        echo '<th class="col-6 table-secondary">Commandé le ' . $commande['dateCommandeF'] . '</th>';
+                        echo '<th class="col-2 table-secondary">' . $commande['etat'] . '</th>';
                         echo '</tr>';
+
+                        $getlignescommandes=$bdd->prepare("SELECT * FROM LignesCommandes,Produits WHERE LignesCommandes.idProduit = Produits.idProduit AND idCommande=?");
+                        $getlignescommandes->execute(array($commande['idCommande']));
+                        $lignescommandesdata=$getlignescommandes->fetchAll();
+
+                        foreach($lignescommandesdata as $lignecommande){
+                            echo '<tr>';
+                            echo '<td class="col-2"><img src="' . $lignecommande['photo'] . '" class="img-fluid d-none d-md-block"></td>';
+                            echo '<td class="col-6">x' . $lignecommande['quantite'] . ' ' . $lignecommande['marque'] . ' - ' . $lignecommande['nom'] . '</td>';
+                            echo '<td class="col-4">Montant : ' . $lignecommande['montant'] . '€</td>';
+                            echo '</tr>';
+                        }
+
+                        echo '</table>';
+                        echo '</div>';
                     }
                 ?>
-            </table>
         </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
