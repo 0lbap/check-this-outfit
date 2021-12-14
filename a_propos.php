fd23f443c9b65800e8d25edb4b3a0b59582be623
Axel C <93790866+Ethazio@users.noreply.github.com>
2021-12-14 16:07:35 +0100

    Update a_propos.php

--- a_propos.php
+++ a_propos.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
-    <title>À propos • Projet Web</title>
+    <title>À propos | check-this-outfit</title>
 </head>
 <body>
     <?php
